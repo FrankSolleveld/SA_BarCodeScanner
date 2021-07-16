32b5503e5086c9646a34c7411f5ab8f8ba123ae6
Frank <fsolleveld@outlook.com>
2021-07-16 15:03:28 +0200

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Frank Solleveld on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
